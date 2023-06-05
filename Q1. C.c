07240b8a5d3f936066026b0dced674f21ed82b75
Qurban100 <135209935+Qurban100@users.noreply.github.com>
2023-06-05 11:24:24 +0500

    Add files via upload

---
+++ Q1. C.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+char name[] = "Qurban Ali ";
+char father_name[] = "M. Ibrahim ";
+int age = 22;
+char grade = 'A';
+
+printf("Name  \t\t:%s\n", name);
+printf("Father's Name   :%s\n", father_name);
+printf("Age  \t\t:%d years\n", age);
+printf("Grade  \t\t:%c\n", grade);
+
+return 0;
+}
+
